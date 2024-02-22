fbe1c3359355ed3e9df837471e43480dbbe9f567
joaoborges99 <borgesvictorj@gmail.com>
2024-02-22 10:21:03 -0300

    widget de navegação

--- lib/report_module/widget/widgets.dart
+++ lib/report_module/widget/widgets.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:package_reports/filtro_module/model/filtros_widget_model.dart';
 import 'package:package_reports/report_module/widget/texto.dart';
 
 class Widgets{
@@ -11,5 +12,78 @@ class Widgets{
       tipo: TipoTexto.titulo,
       cor: cor,
     );
+  }
+
+  navigator({
+    required dynamic pagina, 
+    required BuildContext context, 
+    Function()? onTap, 
+    bool isToShowFiltroNoMeio = false, 
+    required dynamic layout, 
+    required dynamic theme
+  }) async{
+    if(layout.mobile || layout.tablet){
+      await Navigator.push(
+        context, 
+        MaterialPageRoute(
+          builder: (context) => Material( color: Colors.transparent, child: pagina,) 
+        )
+      );
+    }
+    else{
+      switch (pagina.runtimeType) {
+        case const (FiltrosWidgetModel):
+          Widget widgetContrucao = Container(
+            color: Colors.transparent,
+            child: Row(
+              children: isToShowFiltroNoMeio ? 
+              [
+                GestureDetector(
+                  onTap: onTap ?? () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Container(color: Colors.transparent, width: (layout.width - layout.larguraJanelaFiltrosPesquisa)/2,),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 30, bottom: 30),
+                    child: pagina,
+                  )
+                ),
+                GestureDetector(
+                  onTap: onTap ?? () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Container(color: Colors.transparent, width: (layout.width - layout.larguraJanelaFiltrosPesquisa)/2,),
+                ),
+              ] 
+              : 
+              [
+                GestureDetector(
+                  onTap: onTap ?? () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Container(color: Colors.transparent, width: layout.width - layout.larguraJanelaFiltrosPesquisa,),
+                ),
+                Expanded(child: pagina),
+              ],
+            ),
+          );
+          await showDialog<void>(
+            barrierColor: theme.themeDarkAtivo ? Colors.white.withOpacity(0.2) : Colors.black54.withOpacity(0.4),
+            context: context,
+            barrierDismissible: false,
+            builder: (BuildContext context)=> widgetContrucao
+          );
+          break;
+        default:
+          await Navigator.push(
+            context, 
+            MaterialPageRoute(
+              builder: (context) => Material( color: Colors.transparent, child: pagina,) 
+            )
+          );
+      }
+    }
   }
 }
