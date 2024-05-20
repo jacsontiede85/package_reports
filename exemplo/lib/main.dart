7fb063680b48ca78fd0912859e88b72161bc6484
Marlon Santos <marlon-20-12@hotmail.com>
2024-05-20 10:48:18 -0300

    Main

--- exemplo/lib/main.dart
+++ exemplo/lib/main.dart
@@ -7,7 +7,9 @@ void main() {
   SettingsReports().setEnderecoApi(enderecoUrl: 'https://analytics.agnconsultoria.com.br/api/');
   // SettingsReports().setEnderecoApi(enderecoUrl: 'https://api.agnconsultoria.com.br/');
   SettingsReports().setMatricula(matriculaUsu: 3312);
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,42 +20,45 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Teste Report Package',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Teste de Reports Pages'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
-  Widget build(BuildContext context) {  
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text(
+          "Teste de Reports Pages",
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             IconButton.filledTonal(
-              icon: const Icon(CupertinoIcons.doc),
+              icon: const Icon(
+                CupertinoIcons.doc,
+              ),
               onPressed: () {
                 Navigator.push(
-                  context, 
+                  context,
                   MaterialPageRoute(
                     builder: (context) => ReportPage(
                       database: "atacado",
@@ -61,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       function: 'vendas/venda-por-rca/index.php',
                       //sfunction: 'dashboard/query_vendas/leadtime_analitico/index.php',
                     ),
-                  )
+                  ),
                 );
               },
             )
