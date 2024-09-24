522abe5868e8dd8e78681cb3e92ff429c0e95197
joaoborges99 <borgesvictorj@gmail.com>
2024-09-24 16:30:00 -0300

    prints

--- lib/report_module/controller/report_from_json_controller.dart
+++ lib/report_module/controller/report_from_json_controller.dart
@@ -729,26 +729,26 @@ abstract class ReportFromJSONControllerBase with Store, ChangeNotifier {
 }
 
 
-void printW(text) {
-  if (kDebugMode) {
-    print('\x1B[33m$text\x1B[0m');
-  }
-}
+// void printW(text) {
+//   if (kDebugMode) {
+//     print('\x1B[33m$text\x1B[0m');
+//   }
+// }
 
-void printE(text) {
-  if (kDebugMode) {
-    print('\x1B[31m$text\x1B[0m');
-  }
-}
+// void printE(text) {
+//   if (kDebugMode) {
+//     print('\x1B[31m$text\x1B[0m');
+//   }
+// }
 
-void printO(text) {
-  if (kDebugMode) {
-    print('\x1b[32m$text\x1B[0m');
-  }
-}
+// void printO(text) {
+//   if (kDebugMode) {
+//     print('\x1b[32m$text\x1B[0m');
+//   }
+// }
 
-void limparPrint() {
-  if (kDebugMode) {
-    print("\x1B[2J\x1B[0;0H");
-  }
-}
+// void limparPrint() {
+//   if (kDebugMode) {
+//     print("\x1B[2J\x1B[0;0H");
+//   }
+// }
